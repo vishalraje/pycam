fa28eef42dda8fc141da0db67fa9ac59ce4dadc3
hardbyte <hardbyte@b8e30672-3445-11de-b438-597f59cd7555>
2009-05-01 04:47:39 +0000

    Made a general object detector uses haar cascades, made an eye locator. Refactored the conversion from pygame to and from opencv into its own file.

--- src/pygameFaceDetect.py
+++ src/pygameFaceDetect.py
@@ -10,15 +10,14 @@ from VideoCapturePlayer import *
 import faceDetect
 import opencv
 
-#from opencv 
-import adaptors
+from conversionUtils import *
 import pygame
 from pygame.locals import *
 from pygame import surfarray
 
 # While opencv has the image for detecting faces it can paint a rect 
 # around the faces... Note pygame is still rendering the result
-drawWithOpenCv = True
+drawWithOpenCv = False
 
 def drawFacesOnSurface(surf,faces):
     """draw rectangles around detected cvObjects with pygame
@@ -28,13 +27,12 @@ def drawFacesOnSurface(surf,faces):
         r = pygame.Rect(face.x*s,face.y*s,face.width*s,face.height*s)
         pygame.draw.rect(surf,Color("green"),r,1)
     
+
 def locateFacesProcess(surf):
     if drawWithOpenCv:
-        numpyImage = surfarray.pixels3d(surf)
-        cvImage = adaptors.NumPy2Ipl(numpyImage.transpose(1,0,2))
-        faceDetect.detect_and_draw(cvImage)
-        numpyImage = adaptors.Ipl2NumPy(cvImage)
-        surfarray.blit_array(surf,numpyImage.transpose(1,0,2))
+        img = surf2CV(surf)
+        faceDetect.detect_and_draw(img)
+        pygame.surfarray.blit_array(surf,cv2SurfArray(img))
     else:
         faces = getFaces(surf)
         if faces:
@@ -43,7 +41,8 @@ def locateFacesProcess(surf):
     return surf
 
 def getFaces(surf):
-    return faceDetect.detectObject(adaptors.NumPy2Ipl(surfarray.pixels3d(surf).transpose(1,0,2)))
+    img = surf2CV(surf)
+    return faceDetect.detectObject(img)
 
 if __name__ == "__main__":
     vcp = VideoCapturePlayer(processFunction=locateFacesProcess)
