127881febd8f55ae8200507f2c8746b36739bf8b
hardbyte <hardbyte@b8e30672-3445-11de-b438-597f59cd7555>
2009-07-27 04:34:12 +0000

    Added greenscreen threshold example, fixed the command line program detect person, removed .pyc files.

--- trunk/pycam/detectPerson.py
+++ trunk/pycam/detectPerson.py
@@ -7,26 +7,28 @@ from conversionUtils import *
 
 from objectDetect import ObjectDetector
 
-def locatePeopleProcess(surf):
+def locatePeopleProcess(surf, lastState=[]):
     person = False
     for detector in detectors:
         # Try detect anything at all!
         cvMat = surf2CV(surf)
         objects = detector.detectObject(cvMat)
-        if objects:
-            person = True
-            break
-    if person:
-        
-        print "There is a person in front of this computer"
-    else:
-        print "There is noone at this computer"
+        for o in objects:
+            if o:
+                person = True
+                break
+    lastState.append(person)
+    if len(lastState)> 3 and lastState[-1] is not lastState[-2]:
+        if person:
+            print "There is a person in front of this computer"
+        else:
+            print "There is no one at this computer"
 
 if __name__ == "__main__":
 #    eyeDetector = ObjectDetector("eye")
     detectors = [ 
-               #  ObjectDetector("head"),
-              #   ObjectDetector("face"),
+                 ObjectDetector("head"),
+                 ObjectDetector("face"),
                  ObjectDetector("upperbody"),
                  ]
     
---
+++ trunk/pycam/greenscreen.py
@@ -0,0 +1,38 @@
+# Derived from Nirav Patel's Pygame Camera Tutorial
+
+import pygame
+from VideoCapturePlayer import VideoCapturePlayer
+
+class GreenScreen():
+    def __init__(self):
+        self.calibrated = False
+        self.bgs = []
+        
+    def calibrate(self, snapshot):
+        """
+        Capture a bunch of background images and average them out.
+        """
+        if len(self.bgs) < 10:
+            self.bgs.append(snapshot)
+        else:
+            # Average them out to remove noise, and save as background
+            self.background = pygame.transform.average_surfaces(self.bgs)
+            self.calibrated = True 
+        
+    
+    def threshold(self, snapshot):
+        dest = snapshot.copy()
+        pygame.transform.threshold(dest, snapshot, (0,0,0), (100,100,100) ,(255,255,255),1,self.background)   
+        return dest #self.dest
+
+    def process(self, snapshot):
+        if not self.calibrated:
+            return self.calibrate(snapshot)
+        else:
+            return self.threshold(snapshot)
+
+if __name__ == "__main__":
+    greenScreen = GreenScreen()
+    
+    vcp = VideoCapturePlayer(processFunction=greenScreen.process)
+    vcp.main()
