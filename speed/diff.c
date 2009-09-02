279cbd7f8bacad97c5d8f58df53f4099c76e60ac
hardbyte <hardbyte@b8e30672-3445-11de-b438-597f59cd7555>
2009-09-02 04:51:53 +0000

    Gaussian blur examples, opencv VideoCapturePlayer class

--- speed/diff.c
+++ speed/diff.c
@@ -2,10 +2,13 @@
 // g++ -Wall --pedantic `pkg-config --cflags opencv` `pkg-config --libs opencv` -o diff diff.c
 #include "highgui.h"
 #include "cv.h"
-#pragma comment(lib, "cxcore.lib")
+
+//#pragma comment(lib, "cxcore.lib")
+
 #define r(j) differenceImage->imageData[j+2] // red pixel in difference image
 #define g(j) differenceImage->imageData[j+1] // green pixel in difference image
 #define b(j) differenceImage->imageData[j]    // blue pixel in difference image
+
 int main()
 {
     CvCapture* capture         = cvCreateCameraCapture(0);    // connect to a camera
@@ -17,13 +20,20 @@ int main()
     {
         cvCopy( image, previousFrame );                       // backup current frame
         image = cvQueryFrame(capture);                        // get the next frame of video
-        cvAbsDiff( image, previousFrame, differenceImage );   //diff current frame with previous frame
+        cvAbsDiff( image, previousFrame, differenceImage );   // diff current frame with previous frame
+        
         cvThreshold( differenceImage, differenceImage, 32, 0, CV_THRESH_TOZERO );//threshold difference image
-        for(int i=0; i<image->imageSize; i+=3)
-             if (r(i)||g(i)||b(i)) r(i)=g(i)=b(i)=255;        //highlight all remaining difference pixels
+        
+        for(int i=0; i<image->imageSize; i+=3)              // highlight all remaining difference pixels
+             if (r(i)||g(i)||b(i)) r(i)=g(i)=b(i)=255;      // turn off for real pixels from diff image
         
         // median filter out the salt & pepper noise in the difference image
-        cvSmooth(differenceImage, differenceImage, CV_MEDIAN, 3);
+        cvSmooth(differenceImage, differenceImage, CV_MEDIAN, 5);
+        
+        // Filter with gaussian
+        cvSmooth(differenceImage, differenceImage, CV_GAUSSIAN, 3);
+        
+        //cvErode(differenceImage, differenceImage,NULL, 5);
         
         cvShowImage("difference images", differenceImage);    // display highlighted difference image
     }
