bea72a6a90a1b99e25ad8b48dafd2e87ff739185
hardbyte <hardbyte@b8e30672-3445-11de-b438-597f59cd7555>
2009-09-03 09:30:09 +0000

    Added a CPP gaussian blur example, did performance tests of image capture code. Need to look into results, Python shouldn't be faster.

--- speed/blur_scipy.py
+++ speed/blur_scipy.py
@@ -8,10 +8,7 @@ Compile/Run with: python blur_scipy.py
 
 from numpy import array, uint8
 from scipy import signal, ndimage
-
 from VideoCapturePlayer import VideoCapturePlayer as VCP
-
-
 from misc import scipyFromOpenCV
 
 @scipyFromOpenCV
@@ -21,29 +18,9 @@ def gaussianBlur(np_image):
     result = ndimage.filters.gaussian_filter(np_image, (filterSize, filterSize, 1))
     return result
 
-
-from misc import gauss_kern
-
-@scipyFromOpenCV
-def slowGaussianBlur(matrix):
-    """Manual gaussian blur - Very very very slow!"""
-    filterSize = 3
-    filt = gauss_kern(filterSize)
-
-    r = signal.convolve(matrix[:,:,0],filt,'same')
-    g = signal.convolve(matrix[:,:,1],filt,'same')
-    b = signal.convolve(matrix[:,:,2],filt,'same')
-    
-    result = array([r,b,g]).astype(uint8).transpose((1,2,0))
-   
-    return result
-
-
 def main():
     title = "Guassian Filtered Output"
-    vcp = VCP(gaussianBlur,title=title)
-    #vcp = VCP(slowGaussianBlur,title=title)
-    vcp.main()
+    VCP(gaussianBlur,title=title).main()
 
 if __name__ == "__main__": 
     main()
