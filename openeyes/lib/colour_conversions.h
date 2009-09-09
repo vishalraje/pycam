c4a4c86000b47027e59b81ebf6a9291566209647
hardbyte <hardbyte@b8e30672-3445-11de-b438-597f59cd7555>
2009-09-09 14:00:06 +0000

    Took firewire specific code out of cvEyeTracker and into new file. Updated the build system for the example application so both the make based and the scons build work. Fixed a bug I had introduced to the repository with an unsigned variable. Added a README for the example app. Took the colour_conversion code and put into a seperate file.

--- openeyes/lib/colour_conversions.h
+++ openeyes/lib/colour_conversions.h
@@ -1,3 +1,6 @@
+#ifndef CVEYETRACKER_COLOUR_CONV
+#define CVEYETRACKER_COLOUR_CONV
+
 #define YUV2RGB(y, u, v, r, g, b)\
   r = y + ((v*1436) >> 10);\
   g = y - ((u*352 + v*731) >> 10);\
@@ -10,35 +13,5 @@
   b = b > 255 ? 255 : b
 
 //----------------------- uyyvyy (i.e. YUV411) to rgb24 -----------------------//
-void uyyvyy2rgb (unsigned char *src, unsigned char *dest, unsigned long long int NumPixels)
-{
-  register int i = NumPixels + ( NumPixels >> 1 )-1;
-  register int j = NumPixels + ( NumPixels << 1 )-1;
-  register int y0, y1, y2, y3, u, v;
-  register int r, g, b;
-
-  while (i > 0) {
-    y3 = (unsigned char) src[i--];
-    y2 = (unsigned char) src[i--];
-    v  = (unsigned char) src[i--] - 128;
-    y1 = (unsigned char) src[i--];
-    y0 = (unsigned char) src[i--];
-    u  = (unsigned char) src[i--] - 128;
-    YUV2RGB (y3, u, v, r, g, b);
-    dest[j--] = r;
-    dest[j--] = g;
-    dest[j--] = b;
-    YUV2RGB (y2, u, v, r, g, b);
-    dest[j--] = r;
-    dest[j--] = g;
-    dest[j--] = b;
-    YUV2RGB (y1, u, v, r, g, b);
-    dest[j--] = r;
-    dest[j--] = g;
-    dest[j--] = b;
-    YUV2RGB (y0, u, v, r, g, b);
-    dest[j--] = r;
-    dest[j--] = g;
-    dest[j--] = b;
-  }
-}
+void uyyvyy2rgb (unsigned char *src, unsigned char *dest, unsigned long long int NumPixels);
+#endif
---
+++ openeyes/lib/firewire_camera.h
@@ -0,0 +1,54 @@
+/*
+ *      firewire_camera.h
+ *      
+ *      Copyright 2009 Brian Thorne <brian.thorne@canterbury.ac.nz>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+#ifndef CVEYETRACKER_FIREWIRE
+#define CVEYETRACKER_FIREWIRE
+
+#include <libraw1394/raw1394.h>
+#include <libdc1394/dc1394_control.h>
+
+#include "cv.h"
+#include "highgui.h"
+
+/*
+char Feature_Names[9][30] ={
+ "BRIGHTNESS",
+ "EXPOSURE",
+ "SHARPNESS",
+ "WHITE BALANCE",
+ "HUE",
+ "SATURATION",
+ "GAMMA",
+ "SHUTTER",
+ "GAIN"};
+*/
+
+
+// Exposed functions
+int Get_Height();
+int Get_Width();
+void Grab_IEEE1394();
+void Release_IEEE1394();
+void Open_IEEE1394();
+void Close_IEEE1394();
+char *Get_Raw_Frame(unsigned int cam_index);
+void FirewireFrame_to_RGBIplImage(void *FirewireFrame, IplImage *OpenCV_image);
+
+#endif
