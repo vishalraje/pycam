dd44c15378fe6e4900d4cb89f220b2f10f686e81
hardbyte <hardbyte@b8e30672-3445-11de-b438-597f59cd7555>
2009-10-09 13:22:15 +0000

    Added dilation example, decorator for using surfaces and numpy functions

--- trunk/pycam/pycam/conversionUtils.py
+++ trunk/pycam/pycam/conversionUtils.py
@@ -14,3 +14,48 @@ def cv2SurfArray(cvMat):
     """
     numpyImage = adaptors.Ipl2NumPy(cvMat)
     return numpyImage.transpose(1,0,2)
+
+"""
+These decorators could pass on other function args...
+something like: *args=None, **argd=None
+
+"""
+
+class numpyFromSurf(object):
+    """This decorator can be used to wrap a function that takes 
+    and returns a numpy array into one that takes and retuns a
+    pygame surface.
+    """
+    def __init__(self, f):
+        self.f = f  
+    
+    def __call__(self, imageSurface):
+        # Convert surface to ndarray - could also directly acess pixels...
+        np_image = surfarray.array3d(imageSurface)
+        
+        # Call the original function
+        np_image_filtered = self.f(np_image)
+        
+        # Convert back to surface
+        return surfarray.make_surface(np_image_filtered)
+        
+
+
+class numpyFromOpenCV(object):
+    """This decorator can be used to wrap a function that takes 
+    and returns a numpy array into one that takes and retuns an
+    opencv CvMat.
+    """
+    def __init__(self, f):
+        self.f = f    
+
+    def __call__(self, image):
+        # Convert CvMat to ndarray
+        np_image = adaptors.Ipl2NumPy(image)
+        
+        # Call the original function
+        np_image_filtered = self.f(np_image)
+        
+        # Convert back to CvMat
+        return adaptors.NumPy2Ipl(np_image_filtered)
+
