d538c526809d8d93fee5bcc5dc4d7d843cb132a0
hardbyte <hardbyte@b8e30672-3445-11de-b438-597f59cd7555>
2009-12-08 09:57:41 +0000

    OpenCV shouldn't be required - it was.

--- pycam/pycam/conversionUtils.py
+++ pycam/pycam/conversionUtils.py
@@ -1,27 +1,9 @@
-try:
-    from opencv import adaptors
-except ImportError:
-    import adaptors
+
 
 
 from pygame import surfarray
 
 
-def surf2CV(surf):
-    """
-    Given a Pygame surface, convert to an OpenCv cvArray format.
-    Either Ipl image or cvMat.
-    """
-    numpyImage = surfarray.pixels3d(surf)#.copy()    # Is this required to be a copy?
-    cvImage = adaptors.NumPy2Ipl(numpyImage.transpose(1,0,2))
-    return cvImage
-
-def cv2SurfArray(cvMat):
-    """Given an open cvMat convert it to a pygame surface pixelArray
-    Should be able to call blit_array directly on this.
-    """
-    numpyImage = adaptors.Ipl2NumPy(cvMat)
-    return numpyImage.transpose(1,0,2)
 
 """
 These decorators could pass on other function args...
@@ -49,24 +31,49 @@ class numpyFromSurf(object):
         
 
 
-class numpyFromOpenCV(object):
-    """This decorator can be used to wrap a function that takes 
-    and returns a numpy array into one that takes and retuns an
-    opencv CvMat. If the orrientation matters we might need to 
-    transpose (1,0,2)
-    """
-    def __init__(self, f):
-        self.f = f    
 
-    def __call__(self, image):
-        # Convert CvMat to ndarray
-        np_image = adaptors.Ipl2NumPy(image)
-        
-        # Call the original function
-        np_image_filtered = self.f(np_image)
-        
-        # Convert back to CvMat
-        return adaptors.NumPy2Ipl(np_image_filtered)
+
+try:
+    from opencv import adaptors
+    
+    class numpyFromOpenCV(object):
+        """This decorator can be used to wrap a function that takes 
+        and returns a numpy array into one that takes and retuns an
+        opencv CvMat. If the orrientation matters we might need to 
+        transpose (1,0,2)
+        """
+        def __init__(self, f):
+            self.f = f    
+    
+        def __call__(self, image):
+            # Convert CvMat to ndarray
+            np_image = adaptors.Ipl2NumPy(image)
+            
+            # Call the original function
+            np_image_filtered = self.f(np_image)
+            
+            # Convert back to CvMat
+            return adaptors.NumPy2Ipl(np_image_filtered)
+    
+    def surf2CV(surf):
+        """
+        Given a Pygame surface, convert to an OpenCv cvArray format.
+        Either Ipl image or cvMat.
+        """
+        numpyImage = surfarray.pixels3d(surf)#.copy()    # Is this required to be a copy?
+        cvImage = adaptors.NumPy2Ipl(numpyImage.transpose(1,0,2))
+        return cvImage
+
+    def cv2SurfArray(cvMat):
+        """Given an open cvMat convert it to a pygame surface pixelArray
+        Should be able to call blit_array directly on this.
+        """
+        numpyImage = adaptors.Ipl2NumPy(cvMat)
+        return numpyImage.transpose(1,0,2)
+
+    
+except ImportError:
+    pass
 
 try:
 
